8393326cece9a6f7ba7f2c8ad92c36a79504dfba
Satendra Rai <satendrarai200@gmail.com>
2024-12-30 00:25:50 +0530

    add single book page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,15 +2,18 @@ import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
 
 export default async function Home() {
-  
-    // data fetching
 
+  let books; 
+  try {
     const response = await fetch(`${process.env.BACKEND_URL}/books`);
     if(!response.ok) {
       throw new Error('An error occurred while fetching the books');
     }
-
-    const books = await response.json();
+    books = await response.json();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err: unknown) {
+    throw new Error('An error occurred while fetching the books');
+  }
     
   return (
     <>
