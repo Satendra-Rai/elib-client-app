efcfbba02c8523978d4b413900816cf0cc6d6326
Satendra Rai <satendrarai200@gmail.com>
2024-12-30 01:37:14 +0530

    space organization

--- src/app/book/[bookId]/components/DownloadButton.tsx
+++ src/app/book/[bookId]/components/DownloadButton.tsx
@@ -1,19 +1,18 @@
-'use client'
+"use client";
 import React from "react";
 
-const DownloadButton = ({fileLink}: {fileLink: string}) => {
-
-    const handleDownload = () => {
-        window.open(fileLink, '_blank');
-    };
-    return (
-        <button
-            onClick={handleDownload}
-            className="mt-10  h-10 px-4 py-2 rounded-md bg-primary-500 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700"
-        >
-            Download the book
-        </button>
-    );
+const DownloadButton = ({ fileLink }: { fileLink: string }) => {
+  const handleDownload = () => {
+    window.open(fileLink, "_blank");
+  };
+  return (
+    <button
+      onClick={handleDownload}
+      className="mt-10  h-10 px-4 py-2 rounded-md bg-primary-500 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700"
+    >
+      Download the book
+    </button>
+  );
 };
 
 export default DownloadButton;
