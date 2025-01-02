d57de99ac4071e516adc5264939cfaa6319a1ae6
Satendra Rai <satendrarai200@gmail.com>
2025-01-02 21:04:48 +0530

    add loading file

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,10 +2,23 @@ import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = async ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+
+  let books; 
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/books`, { cache: 'no-store' });
+    if(!response.ok) {
+      throw new Error('An error occurred while fetching the books');
+    }
+    books = await response.json();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err: unknown) {
+    throw new Error('An error occurred while fetching the books');
+  }
+
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
-      {books.map((book) => (
+      {books.map((book : Book) => (
         <BookCard key={book._id} book={book} />
       ))}
     </div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,24 +1,16 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default async function Home() {
-
-  let books; 
-  try {
-    const response = await fetch(`${process.env.BACKEND_URL}/books`);
-    if(!response.ok) {
-      throw new Error('An error occurred while fetching the books');
-    }
-    books = await response.json();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err: unknown) {
-    throw new Error('An error occurred while fetching the books');
-  }
     
   return (
     <>
       <Banner />
-      <BookList books={books} />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>    
   );
 }
